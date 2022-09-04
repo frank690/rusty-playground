171b2a92c4933d13ace19c0e2d89f14029605189
frank690 <frank690@sffresch.de>
2022-09-04 10:08:48 +0200

    added PDF for gaussian. next up is sampling method

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,7 +1,6 @@
 // This file holds functions to sample from an arbitrary gaussian distribution
 use rand::{Rng, rngs::ThreadRng, thread_rng};
 
-
 pub struct Gaussian {
     mean: f64,
     variance: f64,
@@ -19,5 +18,10 @@ impl Gaussian {
 
     pub fn sample(&mut self) -> f64 {
         self.random_number_generator.gen::<f64>()
+    }
+
+    pub fn pdf(&self, x: f64) -> f64 {
+        1./f64::sqrt(2.* std::f64::consts::PI * self.variance) * 
+        f64::exp(-f64::powi(x - self.mean, 2) / (2. * self.variance))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod gaussian;
 
 fn main() {
     data::generate(None);
-    let mut g = gaussian::Gaussian::new(1.23, 0.42);
+    let mut g = gaussian::Gaussian::new(0., 1.);
     println!("{}", g.info());
 
     println!("random gaussian sample: {}", g.sample());
+    println!("pdf: {}", g.pdf(-100.));
 }
-
